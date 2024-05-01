ecbf87568528326729e18f7aef82638ce97fcef3
Daniel Broadhurst <danielbroadhurst@theambassadors.com>
2024-05-01 16:58:59 +0100

    feat: nvqs started

--- nvqs/l3-nvq-certificate-in-occupational-health-and-safety/index.php
+++ nvqs/l3-nvq-certificate-in-occupational-health-and-safety/index.php
@@ -24,15 +24,22 @@ require(TEMPLATES_PATH . "/header.php");
   <div class="flex-container">
     <div class="flex-container">
       <div>
-        <div>
-          <p>SEARCH ICON REPLACE</p>
+        <div class="p-10">
+          <span class="nvqs icon blue">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+              <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6 .1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" />
+            </svg>
+          </span>
         </div>
-        <div>
-          <ul>
-            <li>The Level 3 Certificate in Occupational Health and safety is aimed at candidates working as a health and safety practitioner in a position of responsibility for the health, safety and welfare of a team or organisation</li>
-            <li>Is the academic equivalent of the NEBOSH General Certificate</li>
-            <li>Is accepted by the Institution of Occupational Health and Safety (IOSH) for <a href="https://iosh.com/health-and-safety-professionals/become-a-member/membership-categories#technical/" target="_blank">Technical Membership</a> (TechIOSH)*</li>
-            <li><a href="<?= BASE_URL ?>/wp-content/uploads/2023/06/Level-3-NVQ-Certificate-in-Occupational-Health-and-Safety-2023.pdf">Click here to download unit overview</a></li>
+        <div class="p-10">
+          <ul class="nvqs">
+            <path d="M173.9 439.4l-166.4-166.4c-10-10-10-26.2 0-36.2l36.2-36.2c10-10 26.2-10 36.2 0L192 312.7 432.1 72.6c10-10 26.2-10 36.2 0l36.2 36.2c10 10 10 26.2 0 36.2l-294.4 294.4c-10 10-26.2 10-36.2 0z" />
+            </svg> </span>
+            <span class="list-text">
+              <li>The Level 3 Certificate in Occupational Health and safety is aimed at candidates working as a health and safety practitioner in a position of responsibility for the health, safety and welfare of a team or organisation</li>
+              <li>Is the academic equivalent of the NEBOSH General Certificate</li>
+              <li>Is accepted by the Institution of Occupational Health and Safety (IOSH) for <a href="https://iosh.com/health-and-safety-professionals/become-a-member/membership-categories#technical/" target="_blank">Technical Membership</a> (TechIOSH)*</li>
+              <li><a href="<?= BASE_URL ?>/wp-content/uploads/2023/06/Level-3-NVQ-Certificate-in-Occupational-Health-and-Safety-2023.pdf">Click here to download unit overview</a></li>
           </ul>
         </div>
       </div>
@@ -49,9 +56,21 @@ require(TEMPLATES_PATH . "/header.php");
     </div>
     <div class="flex-container">
       <div>
-        <div>
-          <p>PUZZLE ICON REPLACE</p>
-        </div>
+        <div class="p-10">
+        <span class="nvqs icon blue">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path d="M519.4 288.7c-41.5 0-59.5 31.6-82.1 31.6C377.4 320.2 432 144 432 144s-196.3 80-196.3-3.3c0-35.8 36.3-46.3 36.3-86C272 19.2 243.9 0 210.5 0c-34.7 0-66.4 18.9-66.4 56.3 0 41.4 31.7 59.3 31.7 81.8C175.9 207.7 0 166.8 0 166.8v333.2s178.6 41 178.6-28.7c0-22.5-40-40.1-40-81.5 0-37.5 29.3-56.3 63.6-56.3 33.7 0 61.8 19.2 61.8 54.7 0 39.7-36.3 50.2-36.3 86 0 60.8 129.7 25.7 181.2 25.7 0 0-34.7-120.1 25.8-120.1 36 0 46.4 36.2 86.3 36.2C556.7 416 576 388 576 354.4c0-34.2-19-65.8-56.6-65.8z" />
+          </svg>
+        </span>
+      </div>
+            <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6 .1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" />
+          </svg>
+        </span>
+      </div>
+      <div class="p-10">
+        <ul class="nvqs">                <path d="M173.9 439.4l-166.4-166.4c-10-10-10-26.2 0-36.2l36.2-36.2c10-10 26.2-10 36.2 0L192 312.7 432.1 72.6c10-10 26.2-10 36.2 0l36.2 36.2c10 10 10 26.2 0 36.2l-294.4 294.4c-10 10-26.2 10-36.2 0z" />
+              </svg> </span>
+            <span class="list-text">
         <div>
           <ul>
             <li>Candidates must complete all of the Mandatory Units, plus ONE of the Optional Units</li>
--- nvqs/level-2-award-in-first-aid-for-mental-health-rqf/index.php
+++ nvqs/level-2-award-in-first-aid-for-mental-health-rqf/index.php
@@ -10,24 +10,28 @@ require(TEMPLATES_PATH . "/header.php");
 // Header & Menu
 ?>
 
-<section class="container title">
-  <div class="flex-container justify-center">
+<section class="container title mt-30">
+  <div class="flex-container justify-center text-center">
     <h1 class="text-blue">LEVEL 2 AWARD IN FIRST AID FOR MENTAL HEALTH</h1>
   </div>
 </section>
-<section class="container">
-  <div class="flex-container">
-    <h2>About this Level 2 Award</h2>
+<section class="container mt-30">
+  <div class="flex-container justify-center p-10">
+    <h2 class="text-grey">About this Level 2 Award</h2>
   </div>
 </section>
 <section class="container">
   <div class="flex-container">
-    <div class="flex-container">
-      <div>
-        <p>SEARCH ICON REPLACE</p>
+    <div class="flex-container width-50 p-30">
+      <div class="p-10">
+        <span class="nvqs icon blue">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6 .1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" />
+          </svg>
+        </span>
       </div>
-      <div>
-        <ul>
+      <div class="p-10">
+        <ul class="nvqs">
           <li>The Level 2 Award in First Aid for Mental Health is a 1-Day qualification which provides learners with the knowledge to recognise a range of mental health conditions, how to start a supportive conversation, and when and how to signpost a person to seek appropriate professional help</li>
           <li>Successful learners completing this qualification will become a ‘First Aider for Mental Health’ and be able to utilise the skills and knowledge gained within their workplace and private life*</li>
           <li>Learners must be 14 years old or over to undertake this training</li>
@@ -36,13 +40,17 @@ require(TEMPLATES_PATH . "/header.php");
         </ul>
       </div>
     </div>
-    <div class="flex-container">
-      <div>
-        <p>PUZZLE ICON REPLACE</p>
+    <div class="flex-container width-50 p-30">
+      <div class="p-10">
+        <span class="nvqs icon blue">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path d="M519.4 288.7c-41.5 0-59.5 31.6-82.1 31.6C377.4 320.2 432 144 432 144s-196.3 80-196.3-3.3c0-35.8 36.3-46.3 36.3-86C272 19.2 243.9 0 210.5 0c-34.7 0-66.4 18.9-66.4 56.3 0 41.4 31.7 59.3 31.7 81.8C175.9 207.7 0 166.8 0 166.8v333.2s178.6 41 178.6-28.7c0-22.5-40-40.1-40-81.5 0-37.5 29.3-56.3 63.6-56.3 33.7 0 61.8 19.2 61.8 54.7 0 39.7-36.3 50.2-36.3 86 0 60.8 129.7 25.7 181.2 25.7 0 0-34.7-120.1 25.8-120.1 36 0 46.4 36.2 86.3 36.2C556.7 416 576 388 576 354.4c0-34.2-19-65.8-56.6-65.8z" />
+          </svg>
+        </span>
       </div>
-      <div>
-        <h6>By successfully completing this half day course, learners will:</h6>
-        <ul>
+      <div class="p-10">
+        <h6 class="mb-20">By successfully completing this half day course, learners will:</h6>
+        <ul class="nvqs">
           <li>Know what mental health is, why people develop mental health conditions and the roleof a First Aider for Mental Health</li>
           <li>Know how to provide advice and practical support for a person presenting a mentalhealth condition</li>
           <li>Know how to recognise and manage stress</li>
